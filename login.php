1b8a8771acc1bac828f4b4f23cc03a44f62059c5
Davi Cid <davicid123@gmail.com>
2021-02-19 20:14:53 -0300

    Blog do Footstar

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ $erro = isset($_GET["erro"]) ? $_GET["erro"] : 0;
 <head>
     <meta charset="UTF-8">
 
-    <title>Twitter clone</title>
+    <title>Blog Footstar</title>
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,9 +21,9 @@ $erro = isset($_GET["erro"]) ? $_GET["erro"] : 0;
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 
