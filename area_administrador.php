e2c6641b2c0bbee6c28eedd40f148ae08e4e6f30
Davi Cid <davicid123@gmail.com>
2021-02-24 10:33:21 -0300

    Blog do Footstar

--- area_administrador.php
+++ area_administrador.php
@@ -5,3 +5,10 @@ session_start();
 if (!isset($_SESSION["usuario"]) || !($_SESSION["tipo_usuario"] == "administrador")) {
     header("Location: index.php?erro=1");
 }
+
+require_once("estilo_pagina/barra_navegacao.php");
+?>
+
+Ainda em contrução
+
+<?php require_once("estilo_pagina/rodape.php"); ?>
