b93629f7c657871409b53d73495540e1284d2711
Davi Cid <71273627+davicid@users.noreply.github.com>
2023-10-19 09:43:06 -0300

    Update capa.php

--- estilo_pagina/capa.php
+++ estilo_pagina/capa.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<aside class="capa">
+<section class="capa">
     <div class="container">
         <h2><b>JOGUE FOOTSTAR GRÁTIS</b></h2>
         <a href="https://www.footstar.org/">
             <b>Vá para o site do Jogo</b>
         </a>
     </div>
-</aside>
+</section>
